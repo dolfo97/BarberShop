adaade249f553a409ff81787d27519ae735832a4
dolfo97 <110142184+dolfo97@users.noreply.github.com>
2022-12-28 13:31:06 -0700

    updates

--- server.js
+++ server.js
@@ -8,7 +8,16 @@ const logger = require('morgan')
 const bodyParser = require('body-parser')
 const multer = require('multer')
 const PORT = process.env.PORT || 3001
-
+import { fileURLToPath } from "url"
+import authRoutes from ('./routes/auth')
+import userRoutes from ('./routes/users')
+import postRoutes from ('./routes/posts')
+import { register } from "./controllers/auth"
+import { createPost } from "./controllers/posts"
+import { verifyToken } from "./middleware/auth"
+import User from "./models/user"
+import Post from "./models/post"
+import { users, posts } from "./config/seed"
 const app = express()
 
 app.use(express.json())// req.body
@@ -20,17 +29,10 @@ app.use(logger('dev'))
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, 'build')))
 
-// app.use(require('./config/checkToken'))
-// app.use('/api/users', require('./routes/api/users'))
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-// app.use(cors());
 app.use('/images', express.static(path.join(__dirname, 'src/components/images')))
-
-// const ensureLoggedIn = require('./config/ensureLoggedIn')
-// app.use('/api/items', ensureLoggedIn, require('./routes/api/items'))
-// app.use('/api/orders', ensureLoggedIn, require('./routes/api/orders'))
 
 // FILE STORAGE
 const storage = multer.diskStorage({
@@ -43,9 +45,14 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage })
 
-app.get('/api/test', (req, res) => {
-  res.json({ eureka: 'you have found it' })
-})
+/* ROUTES WITH FILES */
+app.post('/auth/register', upload.single("picture"), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes)
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg'
-import './App.css'
+import { register } from '../controllers/auth'
 
 function App () {
   return (
