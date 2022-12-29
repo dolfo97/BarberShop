64bf4f56aed0ec563b76ab9baf5dd825aa1a811a
dolfo97 <110142184+dolfo97@users.noreply.github.com>
2022-12-29 16:07:01 -0700

    new imports

--- server.js
+++ server.js
@@ -1,38 +1,39 @@
-// /server.js
-require('dotenv').config()
-require('./config/database')
-const express = require('express')
-const path = require('path')
-const favicon = require('serve-favicon')
-const logger = require('morgan')
-const bodyParser = require('body-parser')
-const multer = require('multer')
+// require('./config/database')
+import express from "express"
+import bodyParser from "body-parser"
+import path from "path"
+import dotenv from "dotenv"
+// const favicon = require('serve-favicon')
+// const logger = require('morgan')
+// const bodyParser = require('body-parser')
+import multer from "multer"
+import favicon from "serve-favicon"
+import { fileURLToPath } from 'url';
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
+import { register } from "./controllers/auth.js"
+import { createPost } from "./controllers/posts.js"
+import { verifyToken } from "./middleware/auth.js"
 const PORT = process.env.PORT || 3001
-import { fileURLToPath } from "url"
-import authRoutes from ('./routes/auth')
-import userRoutes from ('./routes/users')
-import postRoutes from ('./routes/posts')
-import { register } from "./controllers/auth"
-import { createPost } from "./controllers/posts"
-import { verifyToken } from "./middleware/auth"
-import User from "./models/user"
-import Post from "./models/post"
-import { users, posts } from "./config/seed"
-const app = express()
 
+
+const __filename = fileURLToPath(import.meta.url)
+dotenv.config()
+const app = express()
 app.use(express.json())// req.body
 app.use((req, res, next) => {
   res.locals.data = {}
   next()
 })
-app.use(logger('dev'))
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
-app.use(express.static(path.join(__dirname, 'build')))
+// app.use(logger('dev'))
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
+// app.use(express.static(path.join(__dirname, 'build')))
 
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use('/images', express.static(path.join(__dirname, 'src/components/images')))
+// app.use('/images', express.static(path.join(__dirname, 'src/components/images')))
 
 // FILE STORAGE
 const storage = multer.diskStorage({
@@ -46,13 +47,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 /* ROUTES WITH FILES */
-app.post('/auth/register', upload.single("picture"), register);
-app.post('/posts', verifyToken, upload.single('picture'), createPost);
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-app.use("/posts", postRoutes)
+app.use("/posts", postRoutes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
